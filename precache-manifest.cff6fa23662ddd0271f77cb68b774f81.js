53576a3f77c74fd6aed89a447ae43538da0ee6ec
gijoe2day <gjuliao32@hotmail.com>
2020-08-19 16:27:31 -0500

    Updates

---
+++ precache-manifest.cff6fa23662ddd0271f77cb68b774f81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be1e8b46a50483b4966bddd72c8bea6",
+    "url": "/hollywood-react/index.html"
+  },
+  {
+    "revision": "59fb43c6c4b138b8d918",
+    "url": "/hollywood-react/static/css/main.a6871817.chunk.css"
+  },
+  {
+    "revision": "6ed004f271e22f679826",
+    "url": "/hollywood-react/static/js/2.0faff789.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hollywood-react/static/js/2.0faff789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59fb43c6c4b138b8d918",
+    "url": "/hollywood-react/static/js/main.65db6eec.chunk.js"
+  },
+  {
+    "revision": "a4be9d7c5c7741fac4b1",
+    "url": "/hollywood-react/static/js/runtime-main.435bb27c.js"
+  }
+]);
